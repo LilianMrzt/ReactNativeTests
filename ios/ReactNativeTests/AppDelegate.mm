3d482530777c69251b9807b81804bfdae48866d4
LilianMrzt <lilian.marzet123@gmail.com>
2023-04-02 21:47:53 +0200

    Rename App

--- ios/ReactNativeTests/AppDelegate.mm
+++ ios/ReactNativeTests/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"BaseProject", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"ReactNativeTests", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
